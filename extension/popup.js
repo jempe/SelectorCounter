6004028eba421e26891cc651cb748f0358b6ad0a
Jempe <info@jempe.org>
2023-04-30 13:02:18 -0500

    link counter

--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,16 @@
-document.getElementById('count-button').addEventListener('click', async () => {
-	const cssSelector = document.getElementById('css-selector').value;
-	const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-	const countElements = (selector) => {
-		return document.querySelectorAll(selector).length;
-	};
-
-	try {
-		const result = await chrome.scripting.executeScript({
-			target: { tabId: tab.id },
-			func: countElements,
-			args: [cssSelector],
-		});
-
-		document.getElementById('result').textContent = `Total elements: ${result[0].result}`;
-	} catch (error) {
-		document.getElementById('result').textContent = `Error: ${error.message}`;
-	}
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	chrome.scripting.executeScript(
+		{
+			target: { tabId: tabs[0].id },
+			function: countLinks,
+		},
+		(results) => {
+			document.getElementById("link-count").innerText = results[0].result;
+		}
+	);
 });
+
+function countLinks() {
+	const links = document.getElementsByTagName("a");
+	return links.length;
+}
