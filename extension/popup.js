85bcb8b7ce70aa9b9e7c6497d63656d05a2df365
Jempe <info@jempe.org>
2023-04-30 15:07:10 -0500

    save last selector

--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,31 @@
+// Check the last saved selector and set it as the default value
+chrome.storage.sync.get("lastSelector", (data) => {
+	if (data.lastSelector) {
+		document.getElementById("selector").value = data.lastSelector;
+	}
+});
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	const selector = document.getElementById("selector").value;
+
 	chrome.scripting.executeScript(
 		{
 			target: { tabId: tabs[0].id },
-			function: countLinks,
+			function: countElementsBySelector,
+			args: [selector],
 		},
 		(results) => {
-			document.getElementById("link-count").innerText = results[0].result;
+			document.getElementById("matching-count").innerText = results[0].result;
 		}
 	);
 });
 
 document.getElementById("count-button").addEventListener("click", () => {
 	const selector = document.getElementById("selector").value;
+
+	// Save the entered CSS selector to storage
+	chrome.storage.sync.set({ lastSelector: selector });
+
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 		chrome.scripting.executeScript(
 			{
@@ -25,11 +39,6 @@ document.getElementById("count-button").addEventListener("click", () => {
 		);
 	});
 });
-
-function countLinks() {
-	const links = document.getElementsByTagName("a");
-	return links.length;
-}
 
 function countElementsBySelector(selector) {
 	const elements = document.querySelectorAll(selector);
