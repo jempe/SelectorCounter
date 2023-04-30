fc0689a945feb446aa2a73494ca0d6b63a5c5809
Jempe <info@jempe.org>
2023-04-30 12:16:20 -0500

    version with error: Uncaught (in promise) EvalError: Refused to evaluate a string as JavaScript because unsafe-eval is not an allowed source of script in the following Content Security Policy directive: script-src self

--- extension/popup.js
+++ extension/popup.js
@@ -2,12 +2,19 @@ document.getElementById('count-button').addEventListener('click', async () => {
 	const cssSelector = document.getElementById('css-selector').value;
 	const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-	const code = `document.querySelectorAll('${cssSelector.replace(/'/g, "\\'")}').length`;
+	const countElements = (selector) => {
+		return document.querySelectorAll(selector).length;
+	};
 
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: new Function(code)
-	}, ([result]) => {
-		document.getElementById('result').textContent = `Total elements: ${result.result}`;
-	});
+	try {
+		const result = await chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: countElements,
+			args: [cssSelector],
+		});
+
+		document.getElementById('result').textContent = `Total elements: ${result[0].result}`;
+	} catch (error) {
+		document.getElementById('result').textContent = `Error: ${error.message}`;
+	}
 });
