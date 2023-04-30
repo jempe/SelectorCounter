37c815d7618d6aa9de3b4495ca07fb553f36079d
Jempe <info@jempe.org>
2023-04-30 13:08:39 -0500

    display elements with selector

--- extension/popup.js
+++ extension/popup.js
@@ -10,7 +10,28 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 	);
 });
 
+document.getElementById("count-button").addEventListener("click", () => {
+	const selector = document.getElementById("selector").value;
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		chrome.scripting.executeScript(
+			{
+				target: { tabId: tabs[0].id },
+				function: countElementsBySelector,
+				args: [selector],
+			},
+			(results) => {
+				document.getElementById("matching-count").innerText = results[0].result;
+			}
+		);
+	});
+});
+
 function countLinks() {
 	const links = document.getElementsByTagName("a");
 	return links.length;
+}
+
+function countElementsBySelector(selector) {
+	const elements = document.querySelectorAll(selector);
+	return elements.length;
 }
